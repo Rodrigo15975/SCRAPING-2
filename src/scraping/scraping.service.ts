caa1c45bf4f65f872f266123be20500ba25a4fa0
rodrigo <javierluis15975@hotmail.com>
2025-02-14 23:54:51 -0500

    update scraping.service.ts

--- src/scraping/scraping.service.ts
+++ src/scraping/scraping.service.ts
@@ -1,23 +1,31 @@
-import { EntityManager, MikroORM } from '@mikro-orm/postgresql'
-import { Injectable } from '@nestjs/common'
+import { EntityManager } from '@mikro-orm/postgresql'
+import { Injectable, Logger } from '@nestjs/common'
 import { CreateScrapingDto } from './dto/create-scraping.dto'
 import { UpdateScrapingDto } from './dto/update-scraping.dto'
 import { Scraping } from './entities/scraping.entity'
-
+import { PlaywrightCrawler } from 'crawlee'
 @Injectable()
 export class ScrapingService {
-  constructor(
-    private readonly orm: MikroORM,
-    private readonly em: EntityManager,
-  ) {}
+  constructor(private readonly em: EntityManager) {}
   async create(createScrapingDto: CreateScrapingDto) {
     const scraping = this.em.create(Scraping, createScrapingDto)
     await this.em.persistAndFlush(scraping)
     return scraping
   }
 
-  findAll() {
-    return `This action returns all scraping`
+  async findAll() {
+    const scraping = new PlaywrightCrawler({
+      requestHandler: async ({ page }) => {
+        const title = await page.title()
+        Logger.debug(`Title is '${title}'`)
+      },
+      maxRequestsPerCrawl: 1,
+    })
+    await scraping.run(['https://crawlee.dev'])
+    const data = await scraping.getData()
+    return {
+      data,
+    }
   }
 
   findOne(id: number) {
